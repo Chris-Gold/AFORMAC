70f9e63972ed2e1304a06796c2749d76804157a5
Chris Gold <chris.ratsimba87@gmail.com>
2018-01-22 16:34:46 +0100

    nouvel essai

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<?php
+<form method="post" action="resultat.php">
+	<p>Votre prénom : <input type="text" name="prenom"/></p>
+	<p>Votre nom : <input type="text" name="nom"/></p>
+	<p>Votre adresse : <input type="text" name="adresse"/></p>
+	<p>Votre âge : <input type="text" name="age"/></p>
+	<p>Votre e-mail : <input type="mail" name="mail"/></p>
+	<p>Votre telephone : <input type="text" name="tel"/></p>
+		
+	<p>Vous êtes sur de vous?<input type="checkbox" name="check" value="oui"/></p>
+		
+	<p><input type="submit" name="envoyer" value="Envoyer"/></p>
+</form>
 
-echo "salut teuwah!!";
 
-?>
+
